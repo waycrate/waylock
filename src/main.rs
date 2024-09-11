f297348c0866dd99bfe42b1600fd5a60a1df3de7
sundaram123krishnan <krishnsundaram@gmail.com>
2024-09-11 19:45:05 +0530

    feat: error handling and secure pwd input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use iced::keyboard::key;
-use iced::widget::{column, text_input};
+use iced::widget::{column, text, text_input};
 use iced::window::Id;
 use iced::{keyboard, Command, Element, Event, Renderer, Subscription, Theme};
 use pam::Client;
@@ -7,6 +7,7 @@ use pam::Client;
 struct AuthCredentials {
     name: String,
     password: String,
+    auth_error: String,
 }
 
 use iced_sessionlock::actions::UnLockAction;
@@ -30,6 +31,7 @@ impl MultiApplication for AuthCredentials {
             Self {
                 name: String::new(),
                 password: String::new(),
+                auth_error: String::new(),
             },
             Command::none(),
         )
@@ -45,6 +47,7 @@ impl MultiApplication for AuthCredentials {
                 self.name = name;
                 Command::none()
             }
+
             Message::PasswordEntered(password) => {
                 self.password = password;
                 Command::none()
@@ -61,10 +64,11 @@ impl MultiApplication for AuthCredentials {
                         .conversation_mut()
                         .set_credentials(&self.name, &self.password);
 
-                    if client.authenticate().is_ok() {
-                        return Command::single(UnLockAction.into());
+                    if let Err(auth_error) = client.authenticate() {
+                        self.auth_error = format!("Authentication failed: {}", auth_error);
+                        return Command::none();
                     }
-                    Command::none()
+                    Command::single(UnLockAction.into())
                 }
                 _ => Command::none(),
             },
@@ -77,7 +81,10 @@ impl MultiApplication for AuthCredentials {
     fn view(&self, _window: Id) -> Element<'_, Self::Message, Self::Theme, Renderer> {
         column![
             text_input("Enter name", &self.name).on_input(Message::NameEntered),
-            text_input("Enter password", &self.password).on_input(Message::PasswordEntered),
+            text_input("Enter password", &self.password)
+                .on_input(Message::PasswordEntered)
+                .secure(true),
+            text(&self.auth_error),
         ]
         .into()
     }
